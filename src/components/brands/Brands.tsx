295bb490a783ff9fbf6bedff727d211ac8cf44fe
MohammadAhmadKhader <mohammadkhaderr1999@gmail.com>
2024-03-13 10:30:25 +0200

    adding ternary operator to ensure website wont go down when its not connected to backend side

--- src/components/brands/Brands.tsx
+++ src/components/brands/Brands.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import useAxios from '../customHooks/useAxios'
 import ImageSkeleton from '../shared/LoadingSkeletons/ImageSkeleton'
@@ -26,7 +26,7 @@ function Brands() {
                         <ImageSkeleton customClass={`rounded-2xl col-span-6 lg:col-span-4 xl:col-span-2 p-3  min-h-[150px] lg:min-h-[250px] xl:min-h-[150px]`} />
                         <ImageSkeleton customClass={`rounded-2xl col-span-6 lg:col-span-4 xl:col-span-2 p-3  min-h-[150px] lg:min-h-[250px] xl:min-h-[150px]`} />
                         <ImageSkeleton customClass={`rounded-2xl col-span-6 lg:col-span-4 xl:col-span-2 p-3  min-h-[150px] lg:min-h-[250px] xl:min-h-[150px]`} />
-                    </> : brands.map((brand) => {
+                    </> : brands?.map((brand) => {
                         return (
                             <Link to={`/products?page=1&limit=9&brands=${brand.name}`} className='rounded-2xl bg-gray-200 bg-opacity-80 flex items-center justify-center
                        col-span-6 lg:col-span-4 xl:col-span-2 p-3 aspect-square'
