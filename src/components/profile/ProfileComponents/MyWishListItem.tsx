2729316b42185ca0e6839ada2f8f55f718bcd1a9
MohammadAhmadKhader <mohammadkhaderr1999@gmail.com>
2024-03-29 23:49:54 +0300

    fixing bug with creating payment intent & adding delete review & creating submit review component for (refactory)

--- src/components/profile/ProfileComponents/MyWishListItem.tsx
+++ src/components/profile/ProfileComponents/MyWishListItem.tsx
@@ -7,6 +7,7 @@ import { UserContext } from '../../features/UserFeature/UserProvider';
 import useAxios from '../../customHooks/useAxios';
 import useDebounce from '../../customHooks/useDebounce';
 import { toast } from 'react-toastify';
+import Tooltip from '@mui/material/Tooltip';
 
 function MyWishListItem({ imgUrl, name, productId, wishListId }) {
     const { theme } = useContext(ThemeContext)
@@ -16,7 +17,7 @@ function MyWishListItem({ imgUrl, name, productId, wishListId }) {
 
     const removeFromWishList = async () => {
         try {
-            console.log(userData._id,wishListId)
+            console.log(userData._id, wishListId)
             const response = await DELETE(`/wishlists`, {
                 userId: userData._id,
                 wishlistItemId: wishListId
@@ -41,23 +42,22 @@ function MyWishListItem({ imgUrl, name, productId, wishListId }) {
                 <img src={imgUrl} alt={name} className='rounded-t-md object-contain aspect-square' />
             </div>
             <div className='py-2 px-3'>
-                <div>
-                    <h4 className='text-md font-semibold'>{name}</h4>
-                </div>
-            </div>
+                <div className='flex flex-col justify-between'>
+                    <Tooltip title={name} arrow placement="top-end">
+                        <h4 className='text-md font-semibold line-clamp-1'>{name}</h4>
+                    </Tooltip>
+                    <button className='rounded-md text-sm tracking-wide font-semibold text-white bg-red-600 px-6 py-1 mb-1 mt-2 duration-300 hover hover:bg-red-800'
+                        onClick={(event) => {
+                            debounce(() => { removeFromWishList() }, 200);
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }}>
 
-            <div className='absolute -right-2 -top-2'>
-                <button className='text-red-600 rounded-full bg-black' style={{
-                    backgroundColor: theme == "dark" ? "var(--light--bgCard-color)" : "var(--dark--bgCard-color)",
-                }}
-                    onClick={(event) => {
-                        debounce(() => {removeFromWishList() },200);
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }}
-                >
-                    <MdOutlineCancel size={25} />
-                </button>
+                        <span>
+                            Remove Item
+                        </span>
+                    </button>
+                </div>
             </div>
         </Link>
     )
